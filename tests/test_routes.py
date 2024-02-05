7d78c8bd2ac81967ae3c52f5ca4f44de20dc0cdb
irfan <irfan.roslan.21@ucl.ac.uk>
2024-02-05 16:41:38 +0000

    Add region GET routes to test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,30 @@ def test_get_regions_status_code(client):
     """
     response = client.get("/regions")
     assert response.status_code == 200
+
+def test_get_regions_json(client):
+    """
+    GIVEN a Flask test client
+    AND the database contains data of the regions
+    WHEN a request is made to /regions
+    THEN the response should contain json
+    AND a JSON object for Tonga should be in the json
+    """
+    response = client.get("/regions")
+    assert response.headers["Content-Type"] == "application/json"
+    tonga = {'NOC': 'TGA', 'notes': '', 'region': 'Tonga'}
+    assert tonga in response.json
+
+def test_get_specified_region(client):
+    """
+    GIVEN a Flask test client
+    AND the 5th entry is AND,Andorra,
+    WHEN a request is made to /regions/AND
+    THEN the response json should match that for Andorra
+    AND the response status_code should be 200
+    """
+    and_json = {'NOC': 'AND', 'notes': '', 'region': 'Andorra'}
+    response = client.get("/regions/AND")
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.status_code == 200
+    assert response.json == and_json
