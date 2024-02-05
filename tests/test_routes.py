b736b3a17f3b8ddac6219e77b95a5a282b66392d
irfan <irfan.roslan.21@ucl.ac.uk>
2024-02-05 15:55:03 +0000

    Add code to test_routes.py and .vscode folder

---
+++ tests/test_routes.py
@@ -0,0 +1,8 @@
+def test_get_regions_status_code(client):
+    """
+    GIVEN a Flask test client
+    WHEN a request is made to /regions
+    THEN the status code should be 200
+    """
+    response = client.get("/regions")
+    assert response.status_code == 200
